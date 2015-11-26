0d4cb957888d1621041b88a90c0094fee9104dab
Spring Yang <snbbdx@sina.com>
2015-11-26 17:17:31 +0800

    add modify

--- Source/GateWay/GateWay.Web/Controllers/AccountController.cs
+++ Source/GateWay/GateWay.Web/Controllers/AccountController.cs
@@ -13,5 +13,50 @@ namespace GateWay.Web.Controllers
         {
             return View();
         }
+
+        public ActionResult LogOff()
+        {
+            return View("Login");
+        }
+
+        public ActionResult ConfirmEmail()
+        {
+            return View();
+        }
+
+        public ActionResult ForgotPassword()
+        {
+            return View();
+        }
+
+        public ActionResult ForgotPasswordConfirmation()
+        {
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        public ActionResult ResetPassword()
+        {
+            return View();
+        }
+
+        public ActionResult ResetPasswordConfirmation()
+        {
+            return View();
+        }
+
+        public ActionResult SendCode()
+        {
+            return View();
+        }
+
+        public ActionResult VerifyCode()
+        {
+            return View();
+        }
     }
 }
---
+++ Source/GateWay/GateWay.Web/Helper/Config.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GateWay.Web.Helper
+{
+    public static class Config
+    {
+
+        public static Action<Guid> SaveUserId = delegate (Guid token)
+        {
+            if (HttpContext.Current.Session != null)
+                HttpContext.Current.Session["AccountId"] = token;
+        };
+
+        public static Func<Guid> GetUserId = delegate()
+        {
+            try
+            {
+                var session = HttpContext.Current.Session;
+                if (session != null && session["AccountId"] != null)
+                    return new Guid(session["AccountId"].ToString()) ;
+
+                var controller = HttpContext.Current;
+                var token = controller.Request.Headers["AccountId"] ?? controller.Request.QueryString["AccountId"] ?? controller.Request.Form["AccountId"];
+                return new Guid(token);
+            }
+            catch (Exception ex)
+            {
+                return Guid.Empty;
+            }
+        };
+    }
+}
